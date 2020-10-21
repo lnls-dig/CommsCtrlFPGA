7d3a4a472fcf757c0b8dbacdd3db4453b6a7b254
Lucas Russo <lerwys@gmail.com>
2020-10-21 18:06:47 -0300

    fofb_cc_*_fifo: fix references to .vhd files only
    No need to reference the .xco files. They are here
    for reference only.

--- rtl/fofb_cc_rx_fifo/Manifest.py
+++ rtl/fofb_cc_rx_fifo/Manifest.py
@@ -3,13 +3,13 @@ files = [
 ]
 
 if (target == "xilinx" and syn_device[0:4].upper()=="XC6V"):
-    files.extend(["coregen/virtex6/fofb_cc_rx_fifo.xco"]);
+    files.extend(["coregen/virtex6/fofb_cc_rx_fifo.vhd"]);
 elif (target == "xilinx" and syn_device[0:4].upper()=="XC5V"):
-    files.extend(["coregen/virtex5/fofb_cc_rx_fifo.xco"]);
+    files.extend(["coregen/virtex5/fofb_cc_rx_fifo.vhd"]);
 elif (target == "xilinx" and syn_device[0:4].upper()=="XC6S"):
-    files.extend(["coregen/spartan6/fofb_cc_rx_fifo.xco"]);
+    files.extend(["coregen/spartan6/fofb_cc_rx_fifo.vhd"]);
 elif (target == "xilinx" and syn_device[0:4].upper()=="XC2V"):
-    files.extend(["coregen/virtex2pro/fofb_cc_rx_fifo.xco"]);
+    files.extend(["coregen/virtex2pro/fofb_cc_rx_fifo.vhd"]);
 else:
     import sys
     sys.exit("ERROR: fofb_cc_rx_fifo: Target/Device not supported: {}/{}".format(target, syn_device))
