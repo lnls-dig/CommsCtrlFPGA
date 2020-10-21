1a6f08900a3732429d2bede92ad3eb173b0fb777
Lucas Russo <lerwys@gmail.com>
2020-10-21 18:10:26 -0300

    fofb_cc_rx_fifo: add Manifest support for artix7 generated RX FIFO

--- rtl/fofb_cc_rx_fifo/Manifest.py
+++ rtl/fofb_cc_rx_fifo/Manifest.py
@@ -10,6 +10,8 @@ elif (target == "xilinx" and syn_device[0:4].upper()=="XC6S"):
     files.extend(["coregen/spartan6/fofb_cc_rx_fifo.vhd"]);
 elif (target == "xilinx" and syn_device[0:4].upper()=="XC2V"):
     files.extend(["coregen/virtex2pro/fofb_cc_rx_fifo.vhd"]);
+elif (target == "xilinx" and syn_device[0:4].upper()=="XC7A"):
+    files.extend(["coregen/artix7/fofb_cc_rx_fifo.vhd"]);
 else:
     import sys
     sys.exit("ERROR: fofb_cc_rx_fifo: Target/Device not supported: {}/{}".format(target, syn_device))
