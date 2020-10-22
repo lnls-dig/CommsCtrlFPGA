1c75e290074d0ce8b3023a04d1861275c1247f5a
Lucas Russo <lerwys@gmail.com>
2020-10-22 09:54:49 -0300

    sim/fofb_cc_arbmux: add Manifest for use with hdlmake

---
+++ sim/fofb_cc_arbmux/do/Manifest.py
@@ -0,0 +1,19 @@
+action = "simulation"
+sim_tool = "modelsim"
+top_module = "fofb_cc_arbmux_tb"
+sim_top = "fofb_cc_arbmux_tb"
+
+# needed so we use the correct coregen modules
+target = "xilinx"
+syn_device = "xc7a200t"
+
+modules = {
+    "local" : [
+        "../../../rtl/fofb_cc_arbmux",
+        "../../../rtl/fofb_cc_frame_cntrl",
+        "../../../rtl/fofb_cc_rx_fifo",
+        "../../../rtl/fofb_cc_pkg",
+    ]
+}
+
+files = ["../bench/fofb_cc_arbmux_tb.vhd"]
