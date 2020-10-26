4da341858b9c4f094ca45054e1486a42399f2472
Lucas Russo <lerwys@gmail.com>
2020-10-26 11:27:56 -0300

    rtl/fofb_cc_top: fix adding wrapper files conditionally to variable
    Now, we have the "comms_cc_wrapper" variable that must
    be set if one wants to include a specific wrapper into
    the design.

--- rtl/fofb_cc_top/Manifest.py
+++ rtl/fofb_cc_top/Manifest.py
@@ -1,9 +1,22 @@
 files = [
     "rtl/vhdl/fofb_cc_fifo_rst.vhd",
     "rtl/vhdl/fofb_cc_puls_exp.vhd",
-    "rtl/vhdl/fofb_cc_top_bpm_wrapper.vhd",
-    "rtl/vhdl/fofb_cc_top_sniffer_s6_wrapper.vhd",
-    "rtl/vhdl/fofb_cc_top_sniffer_v5_wrapper.vhd",
-    "rtl/vhdl/fofb_cc_top_sniffer_v6_wrapper.vhd",
     "rtl/vhdl/fofb_cc_top.vhd",
 ]
+
+comms_cc_wrapper_dict = {
+    'bpm_wrapper':          "rtl/vhdl/fofb_cc_top_bpm_wrapper.vhd",
+    'sniffer_s6_wrapper':   "rtl/vhdl/fofb_cc_top_sniffer_s6_wrapper.vhd",
+    'sniffer_v5_wrapper':   "rtl/vhdl/fofb_cc_top_sniffer_v5_wrapper.vhd",
+    'sniffer_v6_wrapper':   "rtl/vhdl/fofb_cc_top_sniffer_v6_wrapper.vhd",
+}
+
+try:
+    if comms_cc_wrapper is not None:
+        for p in comms_cc_wrapper:
+            f = comms_cc_wrapper_dict.get(p, None)
+            assert f is not None, "unknown name {} in 'comms_cc_wrapper'".format(p)
+            files.append(f)
+except NameError:
+    # Do nothing, as nothing needs to be added to the files list
+    pass
