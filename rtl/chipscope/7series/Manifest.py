1186a1e4931e1f25176fae92842cc1c3247a4dcc
Lucas Russo <lerwys@gmail.com>
2021-06-18 11:13:11 -0300

    chipscope/7series: rename components to avoid name clashes
    We make them "local" in the sense no other super-module
    will name their chipscope modules with "fofb_cc".

--- rtl/chipscope/7series/Manifest.py
+++ rtl/chipscope/7series/Manifest.py
@@ -1,5 +1,5 @@
 files = [
-    "ila_t8_d256_s16384/ila_t8_d256_s16384.xci",
-    "ila_t8_d256_s8192_cap/ila_t8_d256_s8192_cap.xci",
-    "ila_t8_d256_s4096_cap/ila_t8_d256_s4096_cap.xci",
+    "ila_fofb_cc_t8_d256_s16384/ila_fofb_cc_t8_d256_s16384.xci",
+    "ila_fofb_cc_t8_d256_s8192_cap/ila_fofb_cc_t8_d256_s8192_cap.xci",
+    "ila_fofb_cc_t8_d256_s4096_cap/ila_fofb_cc_t8_d256_s4096_cap.xci",
 ]
